a8aa24b72d034174655511cae226e5e533d17836
elijahud <udomelijah@protonmail.com>
2026-04-15 14:48:54 +0100

    Pulumi deployment stack

---
+++ sagemkaer/__main__.py
@@ -0,0 +1,65 @@
+import json
+import pulumi
+import pulumi_aws as aws
+
+trust_policy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": { "Service": "sagemaker.amazonaws.com" },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}
+
+sagemaker_role = aws.iam.Role(
+    "sagemakerRole",
+    assume_role_policy=json.dumps(trust_policy),
+    description="Role assumed by SageMaker for inference/training"
+)
+
+# (Optional) attach managed SageMaker policy or your custom policy
+aws.iam.RolePolicyAttachment(
+    "sagemakerManagedAttach",
+    role=sagemaker_role.name,
+    policy_arn="arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+)
+
+pulumi.export("role_arn", sagemaker_role.arn)
+
+## MODEL
+
+sagemaker_model = aws.sagemaker.Model(
+    "sagemakermodel",
+    execution_role_arn=sagemaker_role.arn,
+    primary_container=aws.sagemaker.ModelPrimaryContainerArgs(
+    image="763104351884.dkr.ecr.us-east-1.amazonaws.com/huggingface-pytorch-inference:2.1.0-transformers4.37.0-cpu-py310-ubuntu22.04-v1.4"
+
+    )
+)
+
+## ENDPOINT CONFIG 
+sagemaker_endpoint_configuration = aws.sagemaker.EndpointConfiguration(
+    "sagemakerendpointconfiguration",
+    production_variants=[aws.sagemaker.EndpointConfigurationProductionVariantArgs(
+    instance_type="ml.m5.large",
+    model_name=sagemaker_model.name,
+    variant_name="default",
+    initial_instance_count=1, 
+    )]
+)
+
+## ENDPOINT 
+
+sagemaker_endpoint = aws.sagemaker.Endpoint(
+    "sagemakerendpoint",
+    endpoint_config_name=sagemaker_endpoint_configuration.name
+
+)
+
+## EXPORT Endpoint 
+pulumi.export("endpoint_name", sagemaker_endpoint.name)
+
+
+
